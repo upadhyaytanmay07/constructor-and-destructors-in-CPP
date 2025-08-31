8f3d04834fd60627a34f7ecdc26ac5845cd6156b
upadhyaytanmay07 <tanmayupadhyay1159@gmail.com>
2025-08-31 14:59:24 +0530

    Update destructor function.cpp

--- destructor function.cpp
+++ destructor function.cpp
@@ -4,3 +4,43 @@ ENTC B2 */
 
 // destructor function
 
+#include <iostream>
+using namespace std;
+int count=0;
+class Books
+{
+    string title;
+    string author;
+    float price;
+
+    public:
+    Books()
+    {
+        count++;
+        cout<<"No of objects created: "<<count<<endl;
+    }
+    ~Books()
+    {
+        count--;
+        cout<<"No of objects remaining: "<<count<<endl;
+    }
+};
+int main()
+{
+    Books b1,b2,b3;
+    {
+        Books b4;
+    }
+    return 0;
+}
+
+/*Output
+No of objects created: 1
+No of objects created: 2
+No of objects created: 3
+No of objects created: 4
+No of objects remaining: 3
+No of objects remaining: 2
+No of objects remaining: 1
+No of objects remaining: 0
+*/
